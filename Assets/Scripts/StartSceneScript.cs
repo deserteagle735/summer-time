3cc587fdd5515ec19c06b032bec290b406905f48
deserteagle735 <deserteaglexii@gmail.com>
2019-01-01 02:06:37 +0700

    Start Scene and Path Scene

---
+++ Assets/Scripts/StartSceneScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartSceneScript : MonoBehaviour {
+    private GameObject panelStart;
+    private GameObject panelSettings;
+
+    private void Start()
+    {
+        panelStart = GameObject.Find("PanelStart");
+        panelSettings = GameObject.Find("PanelSettings");
+    }
+
+    public void ButtonSettings()
+    {
+        panelStart.SetActive(false);
+        panelSettings.SetActive(true);
+    }
+
+    public void ButtonBack()
+    {
+        panelStart.SetActive(true);
+        panelSettings.SetActive(false);
+    }
+
+    public void ButtonPlay() 
+    {
+        TKSceneManager.ChangeScene("PathScene");
+    }
+}
